a2aa497cba8e556841ce032839d5c4b962209250
Rafał Florczak <florczak.raf@gmail.com>
2022-09-01 22:40:07 +0200

    Convert times to local timezone on client side

---
+++ boogiestats/static/boogie_ui/boogiestats.js
@@ -0,0 +1,14 @@
+const dateOptions = {
+    hour: "numeric",
+    minute: "numeric",
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hourCycle: "h24",
+};
+
+[...document.getElementsByClassName("convert-timestamp")].forEach(element => {
+    const datetime = new Date(parseInt(element.textContent) * 1000);
+    element.parentNode.innerHTML = datetime.toLocaleString("en-US", dateOptions);
+})
