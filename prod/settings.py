209eeedf74994dc3004c159a00ad95a5bef5d6d6
Rafał Florczak <florczak.raf@gmail.com>
2023-02-22 22:04:11 +0100

    Delete unused default prod config

--- prod/settings.py
+++
@@ -1,7 +0,0 @@
-from boogiestats.boogiestats.settings import *  # noqa
-
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
-
-BS_CHART_DB_PATH = None  # strongly suggested for the best UX, see boogiestats.boogiestats.settings for details
-BS_LOGO_PATH = None  # static path to a logo
-BS_LOGO_CREDITS = None  # credits for a logo, will be shown in the footer
