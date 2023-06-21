c78a6a323e2fc93e2f42fd0626b5cdb468b9e168
Rafał Florczak <florczak.raf@gmail.com>
2023-06-21 09:21:47 +0200

    Cache new-session view

--- boogiestats/boogie_api/urls.py
+++ boogiestats/boogie_api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from . import views
 
 urlpatterns = [
-    path("new-session.php", views.new_session),
+    path("new-session.php", cache_page(60 * 60)(views.new_session)),
     path("player-scores.php", views.player_scores),
     path("player-leaderboards.php", views.player_leaderboards),
     path("score-submit.php", views.score_submit),
