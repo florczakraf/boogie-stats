be605dab6addb90f2cf6f34f334ef22c5bc18b55
Rafał Florczak <florczak.raf@gmail.com>
2023-07-31 22:35:25 +0200

    Delay redis cache population to speedup startup

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,7 +7,8 @@ export PYTHONPATH="/data"
 django-admin collectstatic --no-input
 django-admin migrate
 
-# populate redis search cache
-/app/docker/populate-redis.py
+# populate redis search cache; delay it in order to lower perceived app startup time
+# if redis uses persistent cache, this won't make any difference
+/bin/bash -c 'sleep 30; /app/docker/populate-redis.py'&
 
 exec gunicorn --bind 0.0.0.0:55523 boogiestats.boogiestats.wsgi --log-level DEBUG --access-logfile /data/access.log --error-logfile /data/error.log --threads "$GUNICORN_THREADS"
