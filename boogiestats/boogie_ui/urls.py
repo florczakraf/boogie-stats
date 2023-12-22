b66f456570575ebccb199fdde12371c5686a8d65
Rafał Florczak <florczak.raf@gmail.com>
2023-12-22 19:05:38 +0100

    Add wrapped stats

--- boogiestats/boogie_ui/urls.py
+++ boogiestats/boogie_ui/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path("players/<int:player_id>/most_played", views.PlayerMostPlayedView.as_view(), name="player_most_played"),
     path("players/<int:player_id>/highscores", views.PlayerHighscoresView.as_view(), name="player_highscores"),
     path("players/<int:player_id>/stats", views.PlayerStatsView.as_view(), name="player_stats"),
+    path("players/<int:player_id>/wrapped/<int:year>", views.PlayerWrappedView.as_view(), name="wrapped"),
     path("players/<int:player_id>/add_rival", views.add_rival, name="add_rival"),
     path("players/<int:player_id>/remove_rival", views.remove_rival, name="remove_rival"),
     path("players/<int:player_id>/day/<str:day>", views.PlayerScoresByDayView.as_view(), name="player_scores_by_day"),
