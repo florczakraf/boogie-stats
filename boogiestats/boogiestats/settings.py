fdd2dfad3059c7b61b1c3feac9f9f4c9cb986a3b
Rafał Florczak <florczak.raf@gmail.com>
2023-12-26 16:13:39 +0100

    Bump Django to 5

--- boogiestats/boogiestats/settings.py
+++ boogiestats/boogiestats/settings.py
@@ -131,7 +131,11 @@ STATIC_ROOT = "./static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.ManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
